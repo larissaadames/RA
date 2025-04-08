81cb38738718a3fdf834906bb75a2a75c1c4c658
Larissa Adames <larissaadames0@gmail.com>
2025-04-08 01:55:42 -0300

    mlsam,lasdf

---
+++ ExSem5.py
@@ -0,0 +1,47 @@
+# print("Seja bem-vindo!!")
+# print(" 1. Soma \n 2. Subtração \n 3. Divisão \n 4. Multiplicação \n 5. Sair")
+
+# opcao = int(input("Escolha uma opção! "))
+
+# valorA = float(input("Insira o primeiro valor: "))
+# valorB = float(input("Insira o segundo valor: "))
+
+# if opcao == 1:
+#     resul1 = valorA + valorB
+#     print(resul1)
+# elif opcao == 2:
+#     result2 = valorA - valorB 
+#     print(result2)
+# elif opcao == 3:
+#     result3 = valorA / valorB
+#     print(result3)
+# elif opcao == 4:
+#     result4 = valorA * valorB
+#     print(result4)
+
+# else:
+#     print("Até a próxima!")
+#     exit();
+
+
+
+# 2. Você está em uma floresta e precisa escolher um caminho para seguir. Existem duas opções: o caminho da esquerda e o caminho da direita.
+# Se você escolher o caminho da esquerda, irá encontrar um rio e precisará decidir se quer tentar atravessá-lo ou não. 
+# Se escolher o caminho da direita, irá encontrar uma montanha e precisará decidir se quer subi-la ou não. 
+# Crie um programa em Python que simule essa situação e apresente o resultado final.
+
+
+print("Você está em uma floresta e precisa escolher um caminho! \n O caminho da esquerda tem um rio, e o da direita tem uma montanha")
+caminho = input("qual caminho você escolhe?").lower
+
+if caminho == "esquerda":
+    print("rio")
+    # input("sfjhkasd").lower
+    # if sim:
+    #     print("fds")
+elif caminho == "direita":
+    print("montanha subir?")
+    # if subir == 1:
+    #     print("pena q fds")
+    # else:
+    #     print("porra marina")
--- Exercícios Semana 3.py
+++ Exercícios Semana 3.py
@@ -1,64 +1,64 @@
-##treino da aula
-##nome = input('Qual o seu nome?')
-##cpf = input('Qual o seu cpf?')
-##telefone = input('Qual o seu telefone?')
-##anoNascimento = input('Qual a seu ano de nascimento?')
+# ##treino da aula
+# ##nome = input('Qual o seu nome?')
+# ##cpf = input('Qual o seu cpf?')
+# ##telefone = input('Qual o seu telefone?')
+# ##anoNascimento = input('Qual a seu ano de nascimento?')
 
-##print('Seu nome é: ' + nome)
-##print('Seu cpf é: ' + cpf)
-##print(telefone)
-##print(anoNascimento)
+# ##print('Seu nome é: ' + nome)
+# ##print('Seu cpf é: ' + cpf)
+# ##print(telefone)
+# ##print(anoNascimento)
 
-## treino
-##peso = int(input('Qual o seu peso?'))
-# altura = float(input('Qual a sua altura?'))
-# imc=(peso/(altura * altura))
-# print(imc)
+# ## treino
+# ##peso = int(input('Qual o seu peso?'))
+# # altura = float(input('Qual a sua altura?'))
+# # imc=(peso/(altura * altura))
+# # print(imc)
 
-#ex1
-# anoNascimento = int(input('Insira o seu ano de nascimento: '))
-# idade = (2025 - anoNascimento)
-# print('Sua idade é: ' + str(idade))
+# #ex1
+# # anoNascimento = int(input('Insira o seu ano de nascimento: '))
+# # idade = (2025 - anoNascimento)
+# # print('Sua idade é: ' + str(idade))
 
-#ou
+# #ou
 
-# anoNascimento = int(input('Insira o seu ano de nascimento: '))
-# anoAtual = int(input('Insira o ano atual: '))
-# idade = (anoAtual - anoNascimento)
-# print('A sua idade é: ' + str(idade))
+# # anoNascimento = int(input('Insira o seu ano de nascimento: '))
+# # anoAtual = int(input('Insira o ano atual: '))
+# # idade = (anoAtual - anoNascimento)
+# # print('A sua idade é: ' + str(idade))
 
-#ex2
+# #ex2
 
-# valorCarro = 100
-# carrosCliente = (int(input('Qual é a quantidade de carros desejada? ')))
-# valorFinal = (valorCarro * carrosCliente)
-# print('O valor final ficou: ' + str(valorFinal))
+# # valorCarro = 100
+# # carrosCliente = (int(input('Qual é a quantidade de carros desejada? ')))
+# # valorFinal = (valorCarro * carrosCliente)
+# # print('O valor final ficou: ' + str(valorFinal))
 
-#ex3
+# #ex3
 
-# tempCelcius = float(input('Qual é a temperatura em Celcius?'))
-# tempFa = ((tempCelcius * (9/5))+35)
-# print('Temperatura em Fahrenheit: ' + str(tempFa))
+# # tempCelcius = float(input('Qual é a temperatura em Celcius?'))
+# # tempFa = ((tempCelcius * (9/5))+35)
+# # print('Temperatura em Fahrenheit: ' + str(tempFa))
 
 
-#ex4
-# nota1 = float((input('Qual a sua nota 1? ')))
-# nota2 = float((input('Qual a sua nota 2? ')))
-# nota3 = float((input('Qual a sua nota 3? ')))
-# nota4 = float((input('Qual a sua nota 4? ')))
+# #ex4
+# # nota1 = float((input('Qual a sua nota 1? ')))
+# # nota2 = float((input('Qual a sua nota 2? ')))
+# # nota3 = float((input('Qual a sua nota 3? ')))
+# # nota4 = float((input('Qual a sua nota 4? ')))
 
-# media = ((nota1 + nota2 + nota3 + nota4) / 4)
-# print('A sua média é: '+ str(media))
+# # media = ((nota1 + nota2 + nota3 + nota4) / 4)
+# # print('A sua média é: '+ str(media))
 
-#ex5 
+# #ex5 
 
-# idade = int(input('Insira a sua idade: '))
-# idadeMeses = (idade * 12)
+# # idade = int(input('Insira a sua idade: '))
+# # idadeMeses = (idade * 12)
 
-# print("A sua idade em meses é: " + str(idadeMeses))
+# # # print("A sua idade em meses é: " + str(idadeMeses))
 
-# #ex6 
-# venda = float(input('Qual o valor do produto?'))
-# kilo = float(input('Qual o kilo do produto?'))
-# valorFinal = (kilo * venda)
-# print( "O valor final é: " + str(valorFinal))
+# # #ex6 
+# # venda = float(input('Qual o valor do produto?'))
+# # kilo = float(input('Qual o kilo do produto?'))
+# # valorFinal = (kilo * venda)
+# # print( "O valor final é: " + str(valorFinal))
