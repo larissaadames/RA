8543b44e073748cfd02ef9d91de8f2c34b3893be
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2025-04-09 21:05:52 -0300

    ra1

---
+++ ExSem6.py
@@ -0,0 +1,6 @@
+numero = int(input("Qual tabuada vc quer?"))
+tabuada = 0
+print(f"A tabuada do {numero} é: ")
+while tabuada <= 10:
+    print(f"{numero} X {tabuada} = {numero * tabuada}")
+    tabuada = tabuada + 1
