4d884ce9789119986e7dc423c6a83651582e55c1
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2025-04-10 02:52:40 -0300

    sem6

--- ExSem6.py
+++ ExSem6.py
@@ -1,6 +1,57 @@
-numero = int(input("Qual tabuada vc quer?"))
-tabuada = 0
-print(f"A tabuada do {numero} é: ")
-while tabuada <= 10:
-    print(f"{numero} X {tabuada} = {numero * tabuada}")
-    tabuada = tabuada + 1
+
+# EX 1
+
+# numero = int(input("Qual tabuada vc quer?"))
+# tabuada = 0
+# print(f"A tabuada do {numero} é: ")
+# while tabuada <= 10:
+#     print(f"{numero} X {tabuada} = {numero * tabuada}")
+#     tabuada = tabuada + 1
+
+#EX 2
+
+# nota = 0
+# while nota < 7:
+#     nota = float(input("Qual a sua nota?"))
+#     if nota < 7:
+#         print("Vc reprovou :( )")
+#     else:
+#         print("Vc foi aprovado! :) ")
+
+#EX 3
+
+# soma = 0
+# numero = float(input("Digite um numero: "))
+# contador = 0
+# while numero != -1 : 
+#     soma += numero
+#     numero = int(input("Digite um numero: "))
+#     contador += 1
+
+# print(soma/contador)
+
+#EX 4   
+
+# n = int(input("Digite quantos numeros quer somar: "))
+# soma = 0
+# contador = 1 
+
+# while contador <= n :
+#     soma += contador
+#     contador += 1
+# print(soma)
+
+# #EX 5 
+# pares = 0
+# impares = 0
+# contador = 0 
+# while contador < 10:
+#     numero = int(input("digite um numero: "))
+
+#     if numero % 2 == 0:
+#         pares += 1
+#     else:
+#         impares += 1 
+#     contador += 1
+# print(f"Pares = {pares}")
+# print(f"Impares = {impares}")
