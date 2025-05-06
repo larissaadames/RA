6f5d987491ae93b148976d90fea381180bf320c0
Larissa Adames <larissaadames0@gmail.com>
2025-05-06 14:43:05 -0300

    ExSem9

---
+++ ExSem9.py
@@ -0,0 +1,16 @@
+# megaSena = ["1","2","3","4","5"]
+# aposta = input("Quais números que vc quer apostar? ")
+# if megaSena == aposta:
+#     print("VC GANHOU!!")
+# else:
+#     print("nao foi dessa vez amigo :/")
+
+a = [1, 0, 5, -2, -5, 7]
+soma = a[0] + a[1] + a[5] 
+print ("essa é a soma: ", soma )
+a[4] = 100
+print(a[0])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
