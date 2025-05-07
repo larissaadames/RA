c5cfd2c2da3ddd381d733fd4c21d8229ed08cf57
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2025-05-07 19:10:01 -0300

    HAHAHA

--- ExSem9.py
+++ ExSem9.py
@@ -1,9 +1,11 @@
-# megaSena = ["1","2","3","4","5"]
-# aposta = input("Quais números que vc quer apostar? ")
-# if megaSena == aposta:
-#     print("VC GANHOU!!")
-# else:
-#     print("nao foi dessa vez amigo :/")
+megaSena = ["1","2","3","4","5"]
+aposta = input("Quais números que vc quer apostar? ") 
+apostaArray = list(aposta)
+
+if megaSena == apostaArray:
+    print("VC GANHOU!!")
+else:
+    print("nao foi dessa vez amigo :/")
 
 # #1.
 # a = [1, 0, 5, -2, -5, 7]
@@ -207,32 +209,32 @@
 
 #11
 
-valores = [0] * 5
-i = 0
+# valores = [0] * 5
+# i = 0
 
-while i < 5:
-    numero = int(input(f"Digite o {i + 1}º valor: "))
-    valores[i] = numero
-    i += 1
+# while i < 5:
+#     numero = int(input(f"Digite o {i + 1}º valor: "))
+#     valores[i] = numero
+#     i += 1
 
-maior = valores[0]
-posMaior= 0
+# maior = valores[0]
+# posMaior= 0
 
-menor = valores[0]
-posMenor = 0
-i = 1
+# menor = valores[0]
+# posMenor = 0
+# i = 1
 
 
-while i < 5:
-    if valores[i] > maior:
-        maior = valores[i]
-        posMaior = i
-    if valores[i] < menor:
-        menor = valores[i]
-        posMenor = i
-    i += 1
+# while i < 5:
+#     if valores[i] > maior:
+#         maior = valores[i]
+#         posMaior = i
+#     if valores[i] < menor:
+#         menor = valores[i]
+#         posMenor = i
+#     i += 1
 
-# Mostra os resultados
-print("\nValores digitados:", valores)
-print(f"Maior valor: {maior} na posição {posMaior}")
-print(f"Menor valor: {menor} na posição {posMenor}")
+
+# print("\nValores digitados:", valores)
+# print(f"Maior valor: {maior} na posição {posMaior}")
+# print(f"Menor valor: {menor} na posição {posMenor}")
