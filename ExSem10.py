bb953cc55b0ac9d46d42728258033ffa38913bb6
Larissa Adames Gonçalves <114102618+larissaadames@users.noreply.github.com>
2025-05-07 21:10:16 -0300

    vla bla clsa

--- ExSem10.py
+++ ExSem10.py
@@ -1,5 +1,15 @@
-for contador in range(11):
-    if contador %2 != 0:
-        print(f"[{contador}] é impar")
-    else:
-        print(f"[{contador}] é par")
+# for contador in range(11):
+#     if contador %2 != 0:
+#         print(f"[{contador}] é impar")
+#     else:
+#         print(f"[{contador}] é par")
+
+# letras = ["p", "r", "o", "f", "e"]
+
+# for i in range(5):
+#     print(letras[i])
+
+letras = ["p", "r", "o", "f", "e"]
+
+for i in letras:
+    print(i)
