058cbc57b1284565be866b2e6b3f167f893e962f
Larissa Adames <larissaadames0@gmail.com>
2025-05-10 01:09:31 -0300

    wow

--- ExSem10.py
+++ ExSem10.py
@@ -29,11 +29,49 @@
 #         print(f"{i} é impar")
 
 #Ex 3
-tab = int(input("A tabuada de que número vc quer? "))
 
-if 1<= tab <= 10:
+# tab = int(input("A tabuada de que número vc quer? "))
 
-    for i in range(1, 11):
-        print(f"{tab} X {i} = {tab * i}")
-else:
-    print("Digite um número válido")
+# if 1<= tab <= 10:
+
+#     for i in range(1, 11):
+#         print(f"{tab} X {i} = {tab * i}")
+# else:
+#     print("Digite um número válido")
+
+#Ex 4
+
+# soma = 0
+# qntde = int(input("Quantas idades você vai digitar? "))
+
+# for i in range(qntde):
+#     idade = int(input(f"Digite a idade {i + 1}: "))
+#     soma += idade
+
+# media = soma / qntde
+# print(f"A média das idades é {media:.2f}")
+
+# Ex 5
+
+# pares = 0
+# impares = 0
+# for i in range(11):
+#     num = int(input("Digite um númer: "))
+#     if i %2 != 0:
+#         impares += 1
+#     else:
+#         pares += 1
+# print(f"Sao {pares} nums pares")
+# print(f"Sao {impares} nums impares")
+
+#Ex 6
+dentro = 0
+fora = 0
+for i in range (11):
+    num = int(input("ASKMDKMDFLKM"))
+    if 10 >=  num <= 20:
+        dentro =+ 1
+    else:
+        fora =+ 1
+print("fora: ",fora)
+print("dentro: ", dentro)
