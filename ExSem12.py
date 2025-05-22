76bf8596d06e36ea51b5fee1bb09523220fcd622
Larissa Adames <larissaadames0@gmail.com>
2025-05-21 22:36:54 -0300

    sem12 aaaaaaaaa

--- ExSem12.py
+++ ExSem12.py
@@ -1,1 +1,30 @@
-##soiefowrjdg
+## 1. Declare uma matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais elementos. Escreva ao final a matriz obtida.
+
+matriz = [
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0],
+[0,0,0,0,0]
+]
+
+for linha in range(5):
+    for coluna in range(5):
+        if linha == coluna:
+            matriz[linha][coluna] = 1
+
+for linha in range(5):
+    print(matriz[linha])
+
+## 2. Leia uma matriz 4 x 4, imprima a matriz e retorne a localização (linha e a coluna) do maior valor.
+
+# matriz = [
+# [0,0,0,0,0],
+# [0,0,0,0,0],
+# [0,0,0,0,0],
+# [0,0,0,0,0]
+# ]
+
+# print(matriz)
+
+# whilhe 
